0004e02a53263310ebafea283196b238574619f5
James Vandersluis <jamesvandersluis@outlook.com>
2025-05-20 08:56:44 -0400

    add: new themes

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,17 @@
 return {
   {
     "lifepillar/vim-solarized8"
-  }
+  },
+  {
+    "ishan9299/modus-theme-vim"
+  },
+  {
+    "nelstrom/vim-mac-classic-theme"
+  },
+  {
+    "yorickpeterse/vim-paper"
+  },
+  {
+    "jvsluis/gedit.nvim"
+  },
 }
